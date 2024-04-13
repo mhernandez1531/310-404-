6b9527e8415a702fd871bfc54646262dcf474f07
Bridget Gorman <birdget22@gmail.com>
2024-04-12 21:10:55 -0500

    syntax errors

--- main.c
+++ main.c
@@ -1,3 +1,24 @@
+#include "cipher.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+int main(int argc, char *argv[]){
+	if(argc != 5) {
+		fprintf(stderr, "usage: %s <mode> <shift> <input file> <output file>\n", arg[0]);
+		return EXIT_FAILURE;
+	}
+	int mode = (strcmp(argv[1], "decrypt") == 0) ? -1 : 1;
+	int shift = atoi(argv[2]) * mode; 
+
+	process_file(argv[3], argv[4], shift);
+	
+	printf("File has been processed successfully.\n");
+	return EXIT_SUCCESS;
+}
+
+
+/*
 START PROGRAM with arguments: mode, shift, input_file, output_file
     IF number of arguments is not exactly 5 THEN
         PRINT usage message
@@ -13,4 +34,4 @@ START PROGRAM with arguments: mode, shift, input_file, output_file
     CALL function process_file with arguments input_file, output_file, and shift
     PRINT confirmation message that file processing was successful
 END PROGRAM
-
+*/
