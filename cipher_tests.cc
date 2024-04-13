5d68107d8ca47a228efa6be487043ed484f02b32
mhernandez1531 <mh0150808@gmail.com>
2024-04-12 22:54:05 -0500

    new ciphertest

--- cipher_tests.cc
+++ cipher_tests.cc
@@ -1,2 +1,16 @@
+TEST(CipherTests, CaesarCipherEncryptDecrypt) {
+    const char* test_string = "Hello, World!";
+    int shift = 3;
 
+    // Test encryption
+    char* encrypted = caesar_cipher_string(test_string, shift);
+    EXPECT_STREQ(encrypted, "Khoor, Zruog!");
 
+    // Test decryption
+    char* decrypted = caesar_cipher_string(encrypted, -shift);
+    EXPECT_STREQ(decrypted, test_string);
+
+    // Clean up memory
+    free(encrypted);
+    free(decrypted);
+}
