15f2ed61a645039235b4305ebfb86ac552fafc62
Bridget Gorman <birdget22@gmail.com>
2024-04-12 19:42:16 -0500

    code for header file

--- cipher.h
+++ cipher.h
@@ -1,4 +1,17 @@
+#ifndef CIPHER_H
+#define CIPHER_H
 
+#include <cypte.h> // for isalpha and islower functions 
+
+//function to apply the caesar cipher on a character 
+char cipher(char ch, int shift);
+
+//function to process the fie for encryption or decryption 
+void process_file(const char *input_path, const char *output_path, int shift);
+
+#endif
+
+/*
 DECLARE function cipher that takes a character and an integer shift
 DECLARE function process_file that takes two string paths (input and output) and an integer shift
-
+*/
