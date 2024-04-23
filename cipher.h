3261867d8da067f42fbc914ac34c206abb5034b3
Bridget Gorman <birdget22@gmail.com>
2024-04-22 21:20:05 -0500

    fixing errors so it will complie

--- cipher.h
+++ cipher.h
@@ -12,15 +12,10 @@ void process_file(const char *input_path, const char *output_path, int shift, ch
 char substitution_cipher(char ch, const char *key, int decrypt);
 void generate_substitution_key(char *key, const char *seed);
 
-#endif /* CIPHER_H */
-
-
-
 #endif
 
 
-/* CIPHER_H */
-
+ /* CIPHER_H */
 /*
 DECLARE function cipher that takes a character and an integer shift
 DECLARE function process_file that takes two string paths (input and output) and an integer shift
