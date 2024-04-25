377ef3c722fbb411fad9deaaa036d3743d20acb5
mhernandez1531 <142850461+mhernandez1531@users.noreply.github.com>
2024-04-24 21:40:26 -0500

    Update cipher.hh
    forgot to add

--- cipher.hh
+++ cipher.hh
@@ -1,29 +1,30 @@
-// cipher.hh
+#ifndef CIPHER_HH
+#define CIPHER_HH
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Other cipher function declarations...
-
 /**
  * Encrypts a message using the Rail Fence Cipher.
  *
- * The message to encrypt.
- * The number of rails to use in the cipher.
- * The encrypted message.
+ * @param message The message to encrypt.
+ * @param rails The number of rails to use in the cipher.
+ * @return The encrypted message.
  */
 char* rail_fence_encrypt(const char* message, int rails);
 
 /**
  * Decrypts a message encrypted with the Rail Fence Cipher.
  *
- * message The message to decrypt.
- * ails The number of rails used in the original encryption.
- * return The decrypted message.
+ * @param message The message to decrypt.
+ * @param rails The number of rails used in the original encryption.
+ * @return The decrypted message.
  */
 char* rail_fence_decrypt(const char* message, int rails);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* CIPHER_HH */
