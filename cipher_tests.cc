09e9d6f246fb5384151fc9f9dafff39bcd94d3b7
Maftab27 <89756157+Maftab27@users.noreply.github.com>
2024-04-24 21:22:51 -0500

    Update cipher_tests.cc with tests for all types

--- cipher_tests.cc
+++ cipher_tests.cc
@@ -1,8 +1,7 @@
 #include <gtest/gtest.h>
+#include "cipher.hh"
 
-#include "cipher_tests.hh"
-
-
+// Test Caesar Cipher Encryption and Decryption
 TEST(CipherTests, CaesarCipherEncryptDecrypt) {
     const char* test_string = "Hello, World!";
     int shift = 3;
@@ -13,6 +12,42 @@ TEST(CipherTests, CaesarCipherEncryptDecrypt) {
 
     // Test decryption
     char* decrypted = caesar_cipher_string(encrypted, -shift);
+    EXPECT_STREQ(decrypted, test_string);
+
+    // Clean up memory
+    free(encrypted);
+    free(decrypted);
+}
+
+// Test Substitution Cipher Encryption and Decryption
+TEST(CipherTests, SubstitutionCipherEncryptDecrypt) {
+    const char* test_string = "Hello, World!";
+    const char* key = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+    // Test encryption
+    char* encrypted = substitution_cipher_encrypt(test_string, key);
+    EXPECT_STREQ(encrypted, "Qvrru, Fxrut!");
+
+    // Test decryption
+    char* decrypted = substitution_cipher_decrypt(encrypted, key);
+    EXPECT_STREQ(decrypted, test_string);
+
+    // Clean up memory
+    free(encrypted);
+    free(decrypted);
+}
+
+// Test Rail Fence Cipher Encryption and Decryption
+TEST(CipherTests, RailFenceCipherEncryptDecrypt) {
+    const char* test_string = "Hello, World!";
+    int rails = 3;
+
+    // Test encryption
+    char* encrypted = rail_fence_cipher_encrypt(test_string, rails);
+    EXPECT_STREQ(encrypted, "Horel,Wl lo!");
+
+    // Test decryption
+    char* decrypted = rail_fence_cipher_decrypt(encrypted, rails);
     EXPECT_STREQ(decrypted, test_string);
 
     // Clean up memory
