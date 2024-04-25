4e48b8404b62c5b02c2c7d1f4b01712c8f23692d
mhernandez1531 <142850461+mhernandez1531@users.noreply.github.com>
2024-04-24 21:30:04 -0500

    Update cipher.hh
    updates

--- cipher.hh
+++ cipher.hh
@@ -9,18 +9,18 @@ extern "C" {
 /**
  * Encrypts a message using the Rail Fence Cipher.
  *
- * @param message The message to encrypt.
- * @param rails The number of rails to use in the cipher.
- * @return The encrypted message.
+ * The message to encrypt.
+ * The number of rails to use in the cipher.
+ * The encrypted message.
  */
 char* rail_fence_encrypt(const char* message, int rails);
 
 /**
  * Decrypts a message encrypted with the Rail Fence Cipher.
  *
- * @param message The message to decrypt.
- * @param rails The number of rails used in the original encryption.
- * @return The decrypted message.
+ * message The message to decrypt.
+ * ails The number of rails used in the original encryption.
+ * return The decrypted message.
  */
 char* rail_fence_decrypt(const char* message, int rails);
 
