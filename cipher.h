e63c73d21585da221c02081d69f7b7a1030ec4d8
mhernandez1531 <142850461+mhernandez1531@users.noreply.github.com>
2024-04-24 21:41:25 -0500

    Update cipher.h
    cipher

--- cipher.h
+++ cipher.h
@@ -1,17 +1,21 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
-#include <ctype.h>
+#include <ctype.h> // for isalpha and islower functions
 
-char cipher(char ch, int shift);
+// Function to apply the caesar cipher on a character
+char caesar_cipher(char ch, int shift);
+
+// Function to process the file for encryption or decryption
 void process_file(const char *input_path, const char *output_path, int shift, char mode);
-char* rail_fence_encrypt(const char* message, int rails);
-char* rail_fence_decrypt(const char* message, int rails);
+
+// Function for the substitution cipher
 char substitution_cipher(char ch, const char *key, int decrypt);
+
+// Function to generate the substitution key
 void generate_substitution_key(char *key, const char *seed);
 
 #endif /* CIPHER_H */
-
 
  /* CIPHER_H */
 /*
