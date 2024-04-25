b84b2e09788f0fd9a71f16985f5dff51b8304649
mhernandez1531 <142850461+mhernandez1531@users.noreply.github.com>
2024-04-24 21:28:51 -0500

    Update cipher.hh
    rail fence cipher add-on

--- cipher.hh
+++ cipher.hh
@@ -1,17 +1,29 @@
-// square_root.hh
-// This header ensures C linkage is enforced when running C++ unit tessts on C code.
+// cipher.hh
 
 #ifdef __cplusplus
-
 extern "C" {
-
-#include "cipher.h"
-
-}
-
-#else
-
-#include "cipher.h"
-
 #endif
 
+// Other cipher function declarations...
+
+/**
+ * Encrypts a message using the Rail Fence Cipher.
+ *
+ * @param message The message to encrypt.
+ * @param rails The number of rails to use in the cipher.
+ * @return The encrypted message.
+ */
+char* rail_fence_encrypt(const char* message, int rails);
+
+/**
+ * Decrypts a message encrypted with the Rail Fence Cipher.
+ *
+ * @param message The message to decrypt.
+ * @param rails The number of rails used in the original encryption.
+ * @return The decrypted message.
+ */
+char* rail_fence_decrypt(const char* message, int rails);
+
+#ifdef __cplusplus
+}
+#endif
